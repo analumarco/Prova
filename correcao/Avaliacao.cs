c157b8d57925cc6eb3d583f80a693b3033116b87
Ana Luísa Marco <syaoranwen@gmail.com>
2023-06-13 14:44:53 -0400

    primeiro commit

---
+++ correcao/Avaliacao.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace correcao
+{
+    public partial class Avaliacao : Form
+    {
+        public Avaliacao()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_avaliar_Click(object sender, EventArgs e)
+        {
+            string[] gabarito = { "A", "B", "D", "E", "C", "A", "A", "D", "E", "B"};
+
+            int contador = 0;
+            
+            if (gabarito[0] == textBox1.Text )
+            {
+                contador++;
+            }
+            if (gabarito[1] == textBox2.Text)
+            {
+                contador++;
+            }
+            if (gabarito[2] == textBox3.Text)
+            {
+                contador++;
+            }
+            if (gabarito[3] == textBox4.Text)
+            {
+                contador++;
+            }
+            if (gabarito[4] == textBox5.Text)
+            {
+                contador++;
+            }
+            if (gabarito[5] == textBox6.Text)
+            {
+                contador++;
+            }
+            if (gabarito[6] == textBox7.Text)
+            {
+                contador++;
+            }
+            if (gabarito[7] == textBox8.Text)
+            {
+                contador++;
+            }
+            if (gabarito[8] == textBox9.Text)
+            {
+                contador++;
+            }
+            if (gabarito[9] == textBox10.Text)
+            {
+                contador++;
+            }
+
+            lb_resultado.Text = "Resultado: " + contador.ToString() + " acertos.";
+
+        }
+    }
+}
---
+++ correcao/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace correcao
+{
+    partial class Correcao
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_correcao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_correcao
+            // 
+            this.bt_correcao.Location = new System.Drawing.Point(60, 50);
+            this.bt_correcao.Name = "bt_correcao";
+            this.bt_correcao.Size = new System.Drawing.Size(147, 65);
+            this.bt_correcao.TabIndex = 0;
+            this.bt_correcao.Text = "Correção";
+            this.bt_correcao.UseVisualStyleBackColor = true;
+            this.bt_correcao.Click += new System.EventHandler(this.bt_correcao_Click);
+            // 
+            // Correcao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 194);
+            this.Controls.Add(this.bt_correcao);
+            this.Name = "Correcao";
+            this.Text = "Correcao";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_correcao;
+    }
+}
+
